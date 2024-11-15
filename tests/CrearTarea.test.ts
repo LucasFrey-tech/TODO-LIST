363dbfbb077b38feb8469d1beb895570999c8299
LucasFrey-tech <lucasfrey19@gmail.com>
2024-11-14 22:19:47 -0300

    Test CrearTarea Terminado

--- tests/CrearTarea.test.ts
+++ tests/CrearTarea.test.ts
@@ -6,25 +6,19 @@ import { Prioridad } from "../src/Enum/Prioridad";
 describe("Test Clase Creacion Tarea", () => {
 
     let creadorTarea:crearTarea;
-    //let tarea = mock<Tarea>;
 
     beforeEach(() =>{
         creadorTarea = new crearTarea();
     });
 
     it("Creando una tarea",() =>{
-        /*tarea = creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241113, Prioridad.ALTA, "TEST", "TAREA");
+        const tarea = creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241114, Prioridad.ALTA, "TEST", "TAREA");
+        expect(tarea).toBeInstanceOf(Tarea);
         expect(tarea.getTitulo()).toBe("Tarea1");
         expect(tarea.getDescripcion()).toBe("Hola");
-        expect(tarea.getFechaVec()).toBe(20241113);
+        expect(tarea.getFechaVec()).toBe(20241114);
         expect(tarea.getPrioridad()).toBe(Prioridad.ALTA);
         expect(tarea.getCategoria()).toBe("TEST");
-        expect(tarea.getEtiqueta()).toBe("TAREA");*/
-        expect(creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241113, Prioridad.ALTA, "TEST", "TAREA").getTitulo()).toBe("Tarea1");
-        expect(creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241113, Prioridad.ALTA, "TEST", "TAREA").getDescripcion()).toBe("Hola");
-        expect(creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241113, Prioridad.ALTA, "TEST", "TAREA").getFechaVec()).toBe(20241113);
-        expect(creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241113, Prioridad.ALTA, "TEST", "TAREA").getPrioridad()).toBe(Prioridad.ALTA);
-        expect(creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241113, Prioridad.ALTA, "TEST", "TAREA").getCategoria()).toBe("TEST");
-        expect(creadorTarea.crearNuevaTarea("Tarea1", "Hola", 20241113, Prioridad.ALTA, "TEST", "TAREA").getEtiqueta()).toBe("TAREA");
+        expect(tarea.getEtiqueta()).toBe("TAREA");
     });
 });
