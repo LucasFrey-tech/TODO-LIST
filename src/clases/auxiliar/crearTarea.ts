38b6ecf41b54b9decb4d56c3ed0e406a953e7d13
LucasFrey-tech <lucasfrey19@gmail.com>
2024-11-18 18:23:46 -0300

    Documentacion de crear tarea

--- src/clases/auxiliar/crearTarea.ts
+++ src/clases/auxiliar/crearTarea.ts
@@ -2,7 +2,17 @@ import { Prioridad } from "../../Enum/Prioridad";
 import ValorNoEncontrado from "../../excepciones/error";
 import Tarea from "../Tarea";
 
+/** * Clase `CrearTarea` que se encarga de crear una nueva tarea. */
 export default class CrearTarea{
+    /** * Crea una nueva tarea. 
+    * @param {string} titulo - El título de la tarea. 
+    * @param {string} descripcion - La descripción de la tarea. 
+    * @param {number} fecha - La fecha de vencimiento de la tarea. 
+    * @param {Prioridad} prioridad - La prioridad de la tarea. 
+    * @param {string} categoria - La categoría de la tarea. 
+    * @param {string} etiqueta - La etiqueta de la tarea. 
+    * @returns {Tarea} - La nueva tarea creada. 
+    * @throws {ValorNoEncontrado} - Si el título está vacío. */
     public crearNuevaTarea(titulo:string, descripcion:string, fecha:number, prioridad:Prioridad, categoria:string, etiqueta:string):Tarea {
         if(!titulo){
             throw new ValorNoEncontrado("El titulo es obligatorio");
